2ac7c74146d7cca6ffb659be701ec72f85eb4878
Tanay PrabhuDesai <tanayseven@gmail.com>
2024-02-09 23:23:59 +0530

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import Greet from './components/Refresh.svelte'
+</script>
+
+<main class="container">
+
+  <div class="row">
+    <Greet />
+  </div>
+
+
+</main>
+
+<style>
+
+</style>
---
+++ src/components/Refresh.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+  import {v4 as uuidv4} from 'uuid'
+
+  let numberOfUuids = 1
+  let selectedSeparator = ","
+
+  let uuid = ""
+
+  const generateUuids = (total: number): string => {
+    let uuids = []
+    for (let i = 0; i < total; i++) {
+      uuids.push(uuidv4())
+    }
+    return uuids.join(selectedSeparator)
+  }
+
+  const refreshUuid = () => {
+    uuid = generateUuids(numberOfUuids)
+  }
+
+  const copy = async () => {
+    await navigator.clipboard.writeText(uuid)
+  }
+
+  const separators = {
+    ",": "Comma separated",
+    "\n": "New line separated",
+    "\t": "Tab separated",
+    " ": "Space separated",
+  }
+
+  uuid = generateUuids(numberOfUuids)
+</script>
+
+<div>
+  <input type="number" bind:value={numberOfUuids} on:keyup={refreshUuid}>
+  <select bind:value={selectedSeparator} on:change={refreshUuid}>
+    {#each Object.entries(separators) as [key, value]}
+      <option value={key} selected={selectedSeparator === key}>{key} {value}</option>
+    {/each}
+  </select>
+  <textarea disabled>{uuid}</textarea><br >
+  <input type="button" on:click={refreshUuid} value="Refresh">
+  <input type="button" on:click={copy} value="Copy">
+</div>
